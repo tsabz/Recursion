3ce4c4d6ca0a01bfcef137c6e5147a115c1cb2a2
Tonia <tonia.saba@gmail.com>
2021-12-17 21:42:48 -0500

    able to run without errors

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,13 +1,42 @@
+import java.util.Scanner;
+
 public class Recursion {
 	
-	public Recursion() {
-		System.out.print("public class");
+	public static int Recursion1(int userSum) 
+	{
 		
-	}
-	
-	public static void main(String[] args) {
-		new Recursion(); 
+		if (userSum == 0) {
+			return userSum;
+			} else {
+			return userSum + Recursion1(userSum - 1);
+			}
 
+	}		
+	
+	public static void main(String[] args) 
+	{
+		int userSum = 0;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Program Started");
+		
+		System.out.println(Recursion1(userSum));
+	
+		int counter = 0;
+		int i;
+		
+		for(i=0;i < 5;i++) {
+			counter = counter + 1;
+			System.out.println(counter);
+			//ask user input 
+			 
+			 System.out.print("Enter any number: ");
+			 userSum = scan.nextInt();
+		}
+	      scan.close();
+	      int sumNum=Recursion1(userSum);
+	      System.out.println("The sum of digits is: "+sumNum);
+	      System.out.println("Scanner Closed.");
 	}
 
 }
