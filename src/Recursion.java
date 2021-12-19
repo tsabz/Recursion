16a26e6be8a982fdfc4ae2b93e0f9f9532bef5ae
Tonia <tonia.saba@gmail.com>
2021-12-19 14:08:40 -0500

    Added notes to code

--- src/Recursion.java
+++ src/Recursion.java
@@ -5,25 +5,41 @@ class Recursion {
     private final Scanner scan = new Scanner(System.in);
 
     public int sum(int count){
-        System.out.print("Enter any number: ");
+        System.out.print("Enter number: ");
         int userSum = scan.nextInt();
+        
+        // if counter equals up to 4 indexes return userSum  
         if(count == 4) {
             scan.close();
             return userSum;
         }
+        // else keep counting 
         count++;
-
+		// return userSum + sum(count); is what makes the function recursive. 
+		// It takes the current value and makes a call to itself with the new count 
+		// (thus asking the user for a new number and so forth). 
         return userSum + sum(count);
     }
 
     public static void main(String[] args)
     {
-       Recursion r = new Recursion();
+    	// creating a recursion object
+    	Recursion recursionObject = new Recursion();
+    	
+		// calls sum on the recursionObject with the initial count value of 0
+    	// so that the value does not affect the count 
+    	int total = recursionObject.sum(0);
 
-       int sumNum =  r.sum(0);
-
-        System.out.println("The sum of digits is: "+sumNum);
+        System.out.println("The total sum is: "+ total);
+       
         System.out.println("Scanner Closed.");
     }
 
 }
+
+
+//References
+//https://www.w3schools.com/java/java_classes.asp
+//https://www.geeksforgeeks.org/arrays-in-java/
+//https://stackoverflow.com/questions/70400733/not-able-to-calculate-sum-in-recursive-java-function?noredirect=1#comment124448102_70400733
+
